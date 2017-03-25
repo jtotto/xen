49e7cc170f10701c8878f5b4f2bc1a906fb95a60
Joshua Otto <jtotto@uwaterloo.ca>
2017-03-24 22:19:18 -0400

    libxc/migration: add try_read_record()
    Enable non-blocking migration record reads by adding a helper routine that
    manages the context of a record read across multiple invocations as the record's
    data becomes available over time.
    
    Signed-off-by: Joshua Otto <xxx>

--- tools/libxc/xc_private.h
+++ tools/libxc/xc_private.h
@@ -384,6 +384,8 @@ int xc_flush_mmu_updates(xc_interface *xch, struct xc_mmu *mmu);
 
 /* Return 0 on success; -1 on error setting errno. */
 int read_exact(int fd, void *data, size_t size); /* EOF => -1, errno=0 */
+/* Like read_exact(), but stores the length read before error to *offset. */
+int try_read_exact(int fd, void *data, size_t size, size_t *offset);
 int write_exact(int fd, const void *data, size_t size);
 int writev_exact(int fd, const struct iovec *iov, int iovcnt);
 
